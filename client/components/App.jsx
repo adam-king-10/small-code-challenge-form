fda4ff759f288595e1c9224ee5582aad9e77fa51
adam-king-10 <adam.t.king10@gmail.com>
2021-09-27 12:25:00 +1300

    Styled form div

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,26 +1,23 @@
 import React, {useState, useEffect} from 'react'
-import {getGreeting} from '../apiClient'
+// import {getGreeting} from '../apiClient'
 import Form from './Form'
-import Output from './Output'
 
 const App = () => {
 
-  const [greeting, setGreeting] = useState('')
-  const [count, setCount] = useState(0)
+  // const [greeting, setGreeting] = useState('')
+  // const [count, setCount] = useState(0)
 
-  useEffect(() => {
-    getGreeting()
-      .then((greeting) => {
-        console.log(greeting)
-        setGreeting(greeting)
-      })
-  })
+  // useEffect(() => {
+  //   getGreeting()
+  //     .then((greeting) => {
+  //       console.log(greeting)
+  //       setGreeting(greeting)
+  //     })
+  // })
 
   return (
-    <>
-    {count}
-    <h1>{greeting}</h1>
-    <button onClick={() => setCount(count + 1)}>Click</button>
+    <>  
+     <Form/>
     </>
   )
 }
--- client/components/Form.jsx
+++ client/components/Form.jsx
@@ -3,17 +3,17 @@ import React from 'react';
 function Form () {
 
 return (
-  <div>
+  <div className="formContainer">
     <form>
-      <input type="text" id="name" placeholder="Name" name="fname"/> <br/>
-      <input type="text" id="age" placeholder="Age" name="age"/> <br/>
-      <input type="text" id="email" placeholder="Email" name="email"/>
+      <input type="text" className="input" placeholder="Name" name="fname"/> <br/>
+      <input type="text" className="input" placeholder="Age" name="age"/> <br/>
+      <input type="text" className="input" placeholder="Email" name="email"/>
     </form>
     <div>
       <ul>
-         <li><h2 className="formattribute">Name</h2><h3 className="userinput">{}</h3></li>
-         <li><h2 className="formattribute">Age</h2><h3 className="userinput">{}</h3></li>
-         <li><h2 className="formattribute">Email</h2><h3 className="userinput">{}</h3></li>
+         <li><h2 className="formAttribute">Name</h2><h3 className="userInput">{}</h3></li>
+         <li><h2 className="formAttribute">Age</h2><h3 className="userInput">{}</h3></li>
+         <li><h2 className="formAttribute">Email</h2><h3 className="userInput">{}</h3></li>
       </ul>
     </div>
   </div>
