b9cb60c39b4e2b89b3dc8cfb5dbc7a03631e9aa7
adam-king-10 <adam.t.king10@gmail.com>
2021-09-27 14:22:21 +1300

    changed UL to table

--- client/components/Form.jsx
+++ client/components/Form.jsx
@@ -9,10 +9,6 @@ function Form (props) {
   }
   const [input, setInput] = useState(defaultState)
 
-  useEffect(() => {
-
-  })
-
   const handleChange = evt => {
     const { name, value } = evt.target
     setInput({
@@ -28,15 +24,27 @@ return (
       <input type="text" className="input" placeholder="Age" name="age" value={input.age} onChange={handleChange}/> <br/>
       <input type="text" className="input" placeholder="Email" name="email"value={input.email} onChange={handleChange}/>
     </form>
-    <div>
-      <ul>
-         <li><h2 className="formAttribute">Name</h2>{input.name}</li>
-         <li><h2 className="formAttribute">Age</h2><h3 className="userInput">{input.age}</h3></li>
-         <li><h2 className="formAttribute">Email</h2><h3 className="userInput">{input.email}</h3></li>
-      </ul>
+    <div className="tableContainer">
+    <table>
+      <tbody>
+        <tr>
+          <th className="formItem">Name</th>
+          <td className="userInput">{input.name}</td>
+        </tr>
+        <tr>
+          <th className="formItem">Age</th>
+          <td className="userInput">{input.age}</td>
+        </tr>
+        <tr>
+          <th className="formItem">Email</th>
+          <td className="userInput">{input.email}</td>
+        </tr>
+      </tbody>
+    </table>
     </div>
   </div>
   )
 }
+
 
 export default Form
